2c5231b871031cbff785ac6855c896354f682a83
Dyllan Santos <101805455+DySantos22@users.noreply.github.com>
2022-07-12 18:24:24 -0300

    Delete scripts directory

--- scripts/classValidate.js
+++
@@ -1,7 +0,0 @@
-function validarSenha(){
-    senha = document.formulario.senha.value;
-    confirma_senha = document.formulario.confirma_senha.value;
-    if (confirma_senha != senha){ 
-        alert("SENHAS NÃO SÃO IGUAIS!");
-    }
-}
